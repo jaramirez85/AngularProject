e1ac9af3a2b4eaabda60a265589d35761bb0b89a
JAVIER ALONSO RAMIREZ MONSALVE <jaramirez85@gmail.com>
2018-12-03 20:27:52 -0500

    Template Driven Form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 import { LoginComponent } from './containers/login/login.component';
 import { routes } from "./routes.auth";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ]
 })
--- src/app/auth/containers/login/login.component.ts
+++ src/app/auth/containers/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login, ILogin } from '../../models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login: ILogin;
+
+  constructor() {
+    this.login = new Login();
+  }
 
   ngOnInit() {
+  }
+
+  submit() {
+    console.log(this.login);
   }
 
 }
