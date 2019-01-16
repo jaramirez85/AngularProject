2dedbb51476d9f40e0943949416d7e9b4f470163
Camilo <camilo.castillo.aguas@gmail.com>
2019-01-15 20:52:27 -0500

    Detail - Evolution chain

--- src/app/poke-main/containers/poke-detail/poke-detail.component.ts
+++ src/app/poke-main/containers/poke-detail/poke-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute,Params} from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { PokemonsService } from '../../services/pokemons.service';
+import { copyAnimationEvent } from '@angular/animations/browser/src/render/shared';
 
 
 @Component({
@@ -10,34 +11,81 @@ import { PokemonsService } from '../../services/pokemons.service';
 })
 export class PokeDetailComponent implements OnInit {
 
-  _id:string;
-  _pokemon:any;
-  images:string[] 
-  currentRate:number;
+  _id: string;
+  _pokemon: any;
+  images: string[]
+  abilities: Array<any> = new Array()
+  evolution: Array<any> = new Array()
 
-  constructor(private route:ActivatedRoute,private pokeService:PokemonsService) { }
+  constructor(private route: ActivatedRoute, private pokeService: PokemonsService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
-      (params:Params)=>{
-         this._id=params.id;
+      (params: Params) => {
+        this._id = params.id;
       }
     );
-    this._pokemon=this.pokeService.getPokemonById(this._id).subscribe(
-      (response:any)=>{
-        this._pokemon=response;
-        this.images=[
-                    response.sprites.front_default,
-                    response.sprites.back_default,
-                    response.sprites.front_shiny,
-                    response.sprites.back_shiny
-                  ]
+    this._pokemon = this.pokeService.getPokemonById(this._id).subscribe(
+      (response: any) => {
+        this._pokemon = response;
+        this.images = [
+          response.sprites.front_default,
+          response.sprites.back_default,
+          response.sprites.front_shiny,
+          response.sprites.back_shiny
+        ];
+        this.setAbilities(response);
+        this.setEvolutionChain(response)
       },
-        error=>console.log(`An error has ocurred querying Pokemon with id ${this._id}: ${error}`)
+      error => console.log(`An error has ocurred querying Pokemon with id ${this._id}: ${error}`)
     )
 
+  }
 
+  private setAbilities(response: any) {
+    let abilities: any[] = response.abilities;
+    abilities.forEach(ability => {
+      this.pokeService.getPokemonByUrl(ability.ability.url).subscribe((resp: any) => {
+        this.abilities.push({
+          name: ability.ability.name,
+          description: resp.effect_entries[0].short_effect
+        });
+      })
 
+    });
+  }
+
+  private setEvolutionChain(response: any) {
+    this.pokeService.getPokemonByUrl(response.species.url).subscribe(
+      (r: any) => {
+        this.pokeService.getPokemonByUrl(r.evolution_chain.url).subscribe((evo: any) => {
+          this.processEvolutionChain(evo.chain);
+        })
+      }
+    )
+  }
+
+  private processEvolutionChain(chain:any){
+    this.processSpecie(chain.species);
+    chain.evolves_to.forEach(ch=>{
+      this.processEvolutionChain(ch);
+    })
+  }
+
+  private processSpecie(specie:any){
+    this.pokeService.getPokemonByUrl(specie.url).subscribe(
+      (spec: any) => {
+
+        spec.varieties.forEach(variety => {
+          if (variety.pokemon.name === specie.name) {
+            this.evolution.push({
+              name: variety.pokemon.name,
+              url: variety.pokemon.url
+            })
+          }
+        });
+      }
+    );
   }
 
 }
